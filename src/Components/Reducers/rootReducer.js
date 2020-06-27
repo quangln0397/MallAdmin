b671520616fbbef229bdc7239a28984d91db50cc
your username <quangln0397>
2020-06-27 22:15:13 +0700

    set up redux-thunk, reducer

--- src/Components/Reducers/rootReducer.js
+++ src/Components/Reducers/rootReducer.js
@@ -1,7 +1,13 @@
 import { combineReducers} from 'redux';
 
+const DEFAULT_REDUCER = (initialState, action) => {{
+    return {
+        key: "HELLO WORLD"
+    } 
+}}
+
 const rootReducer = combineReducers({
-    DEFAULT: 'Hello World,'
+    DEFAULT: DEFAULT_REDUCER,
 });
 
 export default rootReducer;
