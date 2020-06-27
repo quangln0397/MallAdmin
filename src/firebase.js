84604b0b33a154add9014c2cfeee139ebfe62c88
your username <quangln0397>
2020-06-27 22:32:53 +0700

    add config firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAD8DqhLT6E0Tgaq0gcQgVbFjBO4DAvTnU",
+    authDomain: "admin-a67ae.firebaseapp.com",
+    databaseURL: "https://admin-a67ae.firebaseio.com",
+    projectId: "admin-a67ae",
+    storageBucket: "admin-a67ae.appspot.com",
+    messagingSenderId: "1060967978906",
+    appId: "1:1060967978906:web:b930f143f5d08cac1acc23",
+    measurementId: "G-TEHP79N82T"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
