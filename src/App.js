dfd747e545dc76d63b8fcfeeb5ac461bfaefdf94
your username <quangln0397>
2020-06-28 10:52:54 +0700

    Up dashboard layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import login from './Pages/login';
+import dashboard from './Pages/dashboard';
+
 
 import { Switch, Route, Redirect } from 'react-router-dom';
 
@@ -12,7 +14,9 @@ function App() {
           <Redirect to="/login"/>
         </Route>
         <Route exact path="/login" component={login} />
+        <Route exact path="/dashboard" component ={dashboard} />
         <Route path="*" render={() => "404 not found!"} />
+
       </Switch>
   );
 }
--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react'
 import { Container, Box, Typography, TextField, CircularProgress, Button } from '@material-ui/core';
+// import firesbase from '../firebase';
 
 import logo from '../media/logo.png'
 
 class login extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            email: "",
+            password: "",
+            show_progress: false
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+
+
+    handleSignIn = () => {
+        this.setState({
+            show_progress: true
+        });
+        setTimeout(() => {
+            this.props.history.push('/dashboard');
+        }, 1000);
+    }
+
     render() {
         return (
-            <Container maxWidth="sm">
+            <Container maxWidth="xs">
                 <Box
                     bgcolor="white"
                     textAlign="center"
                     p="24px"
                     boxShadow="5"
-                    mt="50px"
+                    mt="70px"
                     borderRadius="12px"
                 >
                     <img src={logo} alt="logo" height="50px" />
@@ -28,6 +56,7 @@ class login extends Component {
                         size="small"
                         margin="normal"
                         fullWidth
+                        onChange={this.handleChange}
                     />
 
                     <TextField
@@ -40,13 +69,15 @@ class login extends Component {
                         size="small"
                         margin="normal"
                         fullWidth
+                        onChange={this.handleChange}
                     />
                     <br />
                     <br />
-                    <CircularProgress size={24} thickness={4} color="secondary" />
-                    <Button variant="contained" color="primary" fullWidth>
-                        Primary
-                    </Button>
+                    {this.state.show_progress ? (<CircularProgress size={24} thickness={4} color="secondary" />
+                    ) : <Button variant="contained" color="primary" fullWidth onClick={this.handleSignIn}>
+                            Sign In
+                </Button>}
+
                 </Box>
             </Container>
         );
